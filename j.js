389e8359d0598a6c6bb367873992b938d42f8d70
UsagiKuro269 <cenmodesty@gmail.com>
2025-11-18 14:02:23 +0800

    Add files via upload
    varjs

---
+++ j.js
@@ -0,0 +1,11 @@
+//membuat variabel
+let nilai1 = 100;
+let nilai2 = 200;
+
+let hasil = nilai1 + nilai2;
+var hasil2 = nilai1 + nilai2;
+const phi = 3.14;
+console.log(hasil);
+let nama = "Jenni"
+let umur = 16
+console.log("Halo : " + nama + " Umur Saya adalah : "+ umur + " tahun")
